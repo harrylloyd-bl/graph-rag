572b2c4913b8d4a84e26dab1faa16accd7445f67
harrylloyd-bl <harry.lloyd@bl.uk>
2025-05-12 17:14:56 +0100

    Create duplicate maps

--- graph_rag/dataset.py
+++ graph_rag/dataset.py
@@ -1,7 +1,50 @@
-import logging
+import pickle
 from pathlib import Path
-from tqdm import tqdm
-from graph_rag.config import PROCESSED_DATA_DIR, RAW_DATA_DIR
+import pandas as pd
+from graph_rag.config import PROCESSED_DATA_DIR, RAW_DATA_DIR, MODEL_PARAMS
+from graph_rag.app import load_umap_data
+
+
+def apply_create_map(grp, dup_map: dict):
+    map_to = grp.index[0]
+    for uri in grp.index[1:]:
+        dup_map[uri] = map_to
+    return None
+
+
+def create_duplicate_map(df: pd.DataFrame) -> dict[str, str]:
+    """
+    UMAP is an expensive operation. We de-dup the dataframe of works on "Title" before UMAP-ing to minimise compute time
+    Searches on the database potentially return "Title" duplicates that were dropped.
+    So create a map from dropped duplicate URIs to equivalent URIs represented in the UMAP-ed so we can plot
+    duplicates later.
+    :param df:
+    :return:
+    """
+    duplicates_df = df[df.duplicated(subset="title", keep=False)]
+    duplicates_map = {}
+    duplicates_df.groupby(by="title", as_index=False).apply(apply_create_map, dup_map=duplicates_map,
+                                                            include_groups=False)
+    uris_retained_in_umap = df.drop_duplicates(subset="title", keep="first").index
+    for uri in uris_retained_in_umap:
+        duplicates_map[uri] = uri
+
+    return duplicates_map
+
+
+def validate_save_dup_map(model_params):
+    dup_maps = []
+    for m in model_params:
+        print(m)
+        most_common_lcsh_df, titles_df, embedded_df = load_umap_data(m)
+        combined_df = titles_df.join(embedded_df).dropna()
+        duplicates_map = create_duplicate_map(combined_df)
+        dup_maps.append(duplicates_map)
+
+    if dup_maps[0] == dup_maps[1] and dup_maps[1] == dup_maps[2]:
+        pickle.dump(dup_maps[0], open(f"../static/duplicate_map.p", "wb"))
+
+    return None
 
 
 def main(
@@ -10,14 +53,7 @@ def main(
     output_path: Path = PROCESSED_DATA_DIR / "dataset.csv",
     # ----------------------------------------------
 ):
-    # ---- REPLACE THIS WITH YOUR OWN CODE ----
-    logger = logging.getLogger(__name__)
-    logger.info("Processing dataset...")
-    for i in tqdm(range(10), total=10):
-        if i == 5:
-            logger.info("Something happened for iteration 5.")
-    logger.info("Processing dataset complete.")
-    # -----------------------------------------
+    validate_save_dup_map(MODEL_PARAMS)
 
 
 if __name__ == "__main__":
